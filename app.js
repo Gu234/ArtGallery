8acb3e02e59e04f8234749eb59a8287e02e84054
Gustaw Filipek <guciofilipek@gmail.com>
2019-11-28 19:22:33 +0100

    boxshadow

--- app.js
+++ app.js
@@ -65,36 +65,56 @@ class Gallery extends Component {
   }
 }
 
-const AboutMe = () => <div style={{ padding: '20px', minWidth: '350px', minHeight: '300px', backgroundColor: 'rgba(0, 0, 0, 0.2)', display: 'flex', justifyContent: 'space-around', alignItems: 'center', flexWrap: 'wrap' }}>
+const AboutMe = () =>
   <div style={{
-    backgroundImage: 'url(./images/aboutme.jpg)',
-    padding: '20px',
-    width: '220px', height: '220px', backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    flexShrink: '0'
-  }}></div>
-  <div style={{
-    padding: '20px',
+    backgroundImage: `url(./images/big4.jpg)`,
+    backgroundSize: 'cover', fontFamily: '"Roboto", sans-serif', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center'
+  }}>
+    <Header />
+    <div style={{boxShadow: '3px 3px 20px 0px rgba(0,0,0,1)', padding: '20px', minWidth: '350px', minHeight: '300px', backgroundColor: 'rgba(0, 0, 0, 0.7)', display: 'flex', justifyContent: 'space-around', alignItems: 'center', flexWrap: 'wrap' }}>
+      <div style={{
+        backgroundImage: 'url(./images/aboutme.jpg)',
+        padding: '20px',
+        marginLeft: '20px',
+        width: '220px', height: '220px', backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        flexShrink: '0'
+      }}></div>
+      <div style={{
+        color: 'white',
+        padding: '20px',
 
-    width: '300px', height: '300px',
-    flexShrink: '0'
-  }}>
-    <h2 style={{ textAlign: 'center' }}>About Me</h2>
-    <p style={{ textAlign: 'justify' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident laboriosam incidunt asperiores temporibus porro sapiente sed assumenda delectus, ullam, officiis a laudantium odio voluptas aliquam ducimus doloremque earum perferendis recusandae debitis dolores ratione. Cum a sint illum animi provident eum? Unde illum repellendus, rem animi saepe blanditiis nam reiciendis nesciunt!</p>
+        width: '300px', height: '300px',
+        flexShrink: '0'
+      }}>
+        <h2 style={{ textAlign: 'center' }}>About Me</h2>
+        <p style={{ textAlign: 'justify' }}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident laboriosam incidunt asperiores temporibus porro sapiente sed assumenda delectus, ullam, officiis a laudantium odio voluptas aliquam ducimus doloremque earum perferendis recusandae debitis dolores ratione. Cum a sint illum animi provident eum? Unde illum repellendus, rem animi saepe blanditiis nam reiciendis nesciunt!</p>
+      </div>
+    </div>
+    <Footer />
   </div>
-</div>
-const Contact = () => <div style={{ padding: '20px', minWidth: '350px', minHeight: '300px', backgroundColor: 'rgba(0, 0, 0, 0.2)', display: 'flex', justifyContent: 'space-around', alignItems: 'center', flexWrap: 'wrap' }}>
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2561.770168374606!2d19.94403051529857!3d50.053136579422564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47165b6a94e3a889%3A0x42b468145b2bccc9!2sMiodowa%2C%20Krak%C3%B3w!5e0!3m2!1spl!2spl!4v1574948878596!5m2!1spl!2spl" style={{ width: "400", height: "300", frameborder: "0" }} ></iframe>
+
+const Contact = () =>
   <div style={{
-    display: 'flex', flexDirection: 'column', justifyContent: 'center',
-    width: '300px', height: '300px',
-    flexShrink: '0'
+    backgroundImage: `url(./images/big2.jpg)`,
+    backgroundSize: 'cover', fontFamily: '"Roboto", sans-serif', height: '100vh', display: 'flex', flexDirection: 'column', justifyContent: 'space-between', alignItems: 'center'
   }}>
-    <h4 style={{ textAlign: 'center' }}>Phone:123-123-123</h4>
-    <h4 style={{ textAlign: 'center' }}>Email:lorem@ipsum.com</h4>
-    <h4 style={{ textAlign: 'center' }}>Address: Lorem street 11/23</h4>
+    <Header />
+
+    <div style={{boxShadow: '3px 3px 20px 0px rgba(0,0,0,1)', color: 'white', padding: '20px', minWidth: '350px', minHeight: '300px', backgroundColor: 'rgba(0, 0, 0, 0.7)', display: 'flex', justifyContent: 'space-around', alignItems: 'center', flexWrap: 'wrap' }}>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2561.770168374606!2d19.94403051529857!3d50.053136579422564!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47165b6a94e3a889%3A0x42b468145b2bccc9!2sMiodowa%2C%20Krak%C3%B3w!5e0!3m2!1spl!2spl!4v1574948878596!5m2!1spl!2spl" style={{ marginLeft: '20px', width: "400", height: "300", frameborder: "0" }} ></iframe>
+      <div style={{
+        display: 'flex', flexDirection: 'column', justifyContent: 'center',
+        width: '300px', height: '300px',
+        flexShrink: '0'
+      }}>
+        <h4 style={{ textAlign: 'center' }}>Phone:123-123-123</h4>
+        <h4 style={{ textAlign: 'center' }}>Email:lorem@ipsum.com</h4>
+        <h4 style={{ textAlign: 'center' }}>Address: Lorem street 11/23</h4>
+      </div>
+    </div>
+    <Footer />
   </div>
-</div>
 
 
 const Header = () => <NavLink to='/' style={{ textDecoration: 'none', fontSize: '36px', fontWeight: 'bold', margin: '20px', alignSelf: 'flex-start', color: 'white', paddingLeft: '20px' }}>
@@ -126,7 +146,7 @@ const Home = () =>
   }}>
     <Header />
     <div style={{ maxWidth: "1280px" }}>
-      <div style={{ fontFamily: 'Monospace', fontSize: '30px', minWidth: '300px', height: '40vh', backgroundColor: 'rgba(0, 0, 0, 0.7)', display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center' }}>
+      <div style={{boxShadow: '3px 3px 20px 0px rgba(0,0,0,1)', fontFamily: 'Monospace', fontSize: '30px', minWidth: '300px', height: '40vh', backgroundColor: 'rgba(0, 0, 0, 0.7)', display: 'flex', flexDirection: 'column', justifyContent: 'space-evenly', alignItems: 'center' }}>
         <NavLink style={{ color: 'white', textDecoration: 'none' }} to='/gallery'>Gallery</NavLink>
         <NavLink style={{ color: 'white', textDecoration: 'none' }} to='/aboutme'>About Me</NavLink>
         <NavLink style={{ color: 'white', textDecoration: 'none' }} to='/contact'>Contact</NavLink>
